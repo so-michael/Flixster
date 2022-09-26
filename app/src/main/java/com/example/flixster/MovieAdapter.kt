15c2c02a98fa8bb1ab13b2cabf5e6459b58c8f51
Michael So <mso@michaels-mbp.mynetworksettings.com>
2022-09-26 13:16:20 -0400

    placeholder and rounded corners

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 
@@ -56,8 +57,8 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             }
             Glide.with(context)
                 .load(imageUrl)
-//                .placeholder(R.drawable.ic_launcher_background)
-//                .apply(RequestOptions.bitmapTransform(RoundedCorners(100)))
+                .placeholder(R.drawable.ic_launcher_background)
+                .transform(RoundedCorners(25))
                 .into(ivPoster)
         }
 
